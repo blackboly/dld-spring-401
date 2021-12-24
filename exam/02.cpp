5ab1633dc318fb7742b1b619faf839f27e6e9468
abfzl <ab.eslami2001@gmail.com>
2021-12-24 10:50:08 +0330

    exam FSFA

---
+++ exam/02.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+using namespace std;
+
+int func1(int a){
+
+    for(int i = 1; i <a; i++) {
+        cout << i << " ";
+    }
+    for(int i = a; i > 0; i--) {
+        cout << i << " ";
+    }
+    cout<<endl;
+    return 1;
+}
+
+int main(){
+    int n;
+    cin>>n;
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < (n-i); j++){
+            cout<<" ";
+        }
+        func1(i);
+        for(int j = 1; j < (n-i); j++){
+            cout<<" ";
+        }
+    }
+
+    for(int i = n; i> 0 ; i--){
+        for(int j = 0; j < (n-i); j++){
+            cout<<"  ";
+        }
+
+        func1(i);
+        for(int j = 1; j < (n-i); j++){
+            cout<<" ";
+        }
+    }
+    cout<<endl;
+
+}
