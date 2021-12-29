2ae56781e24dad678b6139fa08cc23986d755896
abfzl <ab.eslami2001@gmail.com>
2021-12-29 14:41:03 +0330

    just a test

--- exam/02.cpp
+++ exam/02.cpp
@@ -18,24 +18,15 @@ int main(){
     cin>>n;
     for(int i = 0; i < n; i++){
         for(int j = 0; j < (n-i); j++){
-            cout<<" ";
+            cout<<"  ";
         }
         func1(i);
-        for(int j = 1; j < (n-i); j++){
-            cout<<" ";
-        }
     }
-
     for(int i = n; i> 0 ; i--){
         for(int j = 0; j < (n-i); j++){
             cout<<"  ";
         }
-
         func1(i);
-        for(int j = 1; j < (n-i); j++){
-            cout<<" ";
-        }
     }
     cout<<endl;
-
 }
